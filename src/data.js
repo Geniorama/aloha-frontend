e269b80b6436367227990e3cde0579cdced25e14
Jair Pérez <oquendojair10@gmail.com>
2023-09-07 08:58:42 -0500

    feat: create data file with planes list

---
+++ src/data.js
@@ -0,0 +1,321 @@
+export const planes = [
+  {
+    id: "fotos-vectores-suscripcion",
+    name: "Suscripción",
+    category: [
+      {
+        name: "Fotos y vectores",
+        slug: "fotos-y-vectores",
+      },
+    ],
+    type: [
+      {
+        id: "pago-anual",
+        name: "Pago anual",
+        subitems: [
+          {
+            name: "10 imágenes",
+            small: "mes",
+            price: 8.99,
+            price_per_item: 0.9,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "75 imágenes",
+            small: "mes",
+            price: 59,
+            price_per_item: 0.79,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "150 imágenes",
+            small: "mes",
+            price: 89,
+            price_per_item: 0.59,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "750 imágenes",
+            small: "mes",
+            price: 169,
+            price_per_item: 0.23,
+            small_per_item: "por imagen",
+          },
+        ],
+      },
+
+      {
+        id: 2,
+        name: "Pago mensual",
+        subitems: [
+          {
+            name: "10 imágenes",
+            small: "mes",
+            price: 8.99,
+            price_per_item: 0.9,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "75 imágenes",
+            small: "mes",
+            price: 59,
+            price_per_item: 0.79,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "150 imágenes",
+            small: "mes",
+            price: 89,
+            price_per_item: 0.59,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "750 imágenes",
+            small: "mes",
+            price: 169,
+            price_per_item: 0.23,
+            small_per_item: "por imagen",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: "fotos-vectores-prepago",
+    name: "Prepago",
+    category: [
+      {
+        name: "Fotos y vectores",
+        slug: "fotos-y-vectores",
+      },
+    ],
+    type: [
+      {
+        id: 1,
+        name: "Pago anual",
+        subitems: [
+          {
+            name: "3 imágenes",
+            small: "mes",
+            price: 42,
+            price_per_item: 14,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "10 imágenes",
+            small: "mes",
+            price: 49,
+            price_per_item: 4.9,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "25 imágenes",
+            small: "mes",
+            price: 99,
+            price_per_item: 3.96,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "25 imágenes",
+            small: "mes",
+            price: 299,
+            price_per_item: 2.99,
+            small_per_item: "por imagen",
+          },
+        ],
+      },
+
+      {
+        id: 2,
+        name: "Pago mensual",
+        subitems: [
+          {
+            name: "10 imágenes",
+            small: "mes",
+            price: 8.99,
+            price_per_item: 0.9,
+            small_per_item: "por imagen",
+          },
+
+          {
+            name: "75 imágenes",
+            small: "mes",
+            price: 59,
+            price_per_item: 0.79,
+            small_per_item: "por imagen",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    name: "Suscripción",
+    category: [
+      {
+        name: "Videos",
+        slug: "videos",
+      },
+    ],
+    type: [
+      {
+        id: 1,
+        name: "Pago anual",
+        subitems: [
+          {
+            name: "10 videos",
+            small: "mes",
+            price: 8.99,
+            price_per_item: 0.9,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "75 videos",
+            small: "mes",
+            price: 59,
+            price_per_item: 0.79,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "150 videos",
+            small: "mes",
+            price: 89,
+            price_per_item: 0.59,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "750 videos",
+            small: "mes",
+            price: 169,
+            price_per_item: 0.23,
+            small_per_item: "por video",
+          },
+        ],
+      },
+
+      {
+        id: 2,
+        name: "Pago mensual",
+        subitems: [
+          {
+            name: "10 videos",
+            small: "mes",
+            price: 8.99,
+            price_per_item: 0.9,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "75 videos",
+            small: "mes",
+            price: 59,
+            price_per_item: 0.79,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "150 videos",
+            small: "mes",
+            price: 89,
+            price_per_item: 0.59,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "750 videos",
+            small: "mes",
+            price: 169,
+            price_per_item: 0.23,
+            small_per_item: "por video",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Prepago",
+    category: [
+      {
+        name: "Videos",
+        slug: "videos",
+      },
+    ],
+    type: [
+      {
+        id: 1,
+        name: "Pago anual",
+        subitems: [
+          {
+            name: "3 imágenes",
+            small: "mes",
+            price: 42,
+            price_per_item: 14,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "10 imágenes",
+            small: "mes",
+            price: 49,
+            price_per_item: 4.9,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "25 imágenes",
+            small: "mes",
+            price: 99,
+            price_per_item: 3.96,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "25 imágenes",
+            small: "mes",
+            price: 299,
+            price_per_item: 2.99,
+            small_per_item: "por video",
+          },
+        ],
+      },
+
+      {
+        id: 2,
+        name: "Pago mensual",
+        subitems: [
+          {
+            name: "10 imágenes",
+            small: "mes",
+            price: 8.99,
+            price_per_item: 0.9,
+            small_per_item: "por video",
+          },
+
+          {
+            name: "75 imágenes",
+            small: "mes",
+            price: 59,
+            price_per_item: 0.79,
+            small_per_item: "por video",
+          },
+        ],
+      },
+    ],
+  },
+];
