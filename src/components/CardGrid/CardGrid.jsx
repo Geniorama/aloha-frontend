04c51de4234c236c238749a5581d553f62f4bed6
Jair Pérez <oquendojair10@gmail.com>
2023-09-07 12:03:16 -0500

    feat(CardGrid): add caption

--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -2,27 +2,24 @@ import ButtonLink from "../ButtonLink/ButtonLink";
 import Image from "next/image";
 import styles from "./CardGrid.module.css";
 
-export default function CardGrid({ items }) {
-  const exampleImages = [
-    "https://images.unsplash.com/photo-1693443760721-93025350bf03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
-    "https://images.unsplash.com/photo-1693443760721-93025350bf03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
-    "https://images.unsplash.com/photo-1693443760721-93025350bf03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
-  ];
+export default function CardGrid({ items = [], name }) {
   return (
-    <div>
-      <div className={`${styles.Grid} ${styles.Grid_3}`}>
-        {exampleImages &&
-          exampleImages.map((item, i) => (
-            <div key={i} className={styles.Item}>
-              <Image 
-                src={item} 
-                alt=""
-                fill={true}
-                >
-                
-               </Image>
-            </div>
-          ))}
+    <div
+      className={`${styles.Grid} ${
+        items.length > 3 ? styles.Grid_4 : styles.Grid_3
+      }`}
+    >
+      {items.length &&
+        items.map((item, i) => (
+          <div key={i} className={styles.Item}>
+            <Image src={item} alt="" fill />
+          </div>
+        ))}
+      <div className={styles.caption}>
+        <h3>
+          Japandi Aesthetic
+          <span> &middot; 300</span>
+        </h3>
       </div>
     </div>
   );
