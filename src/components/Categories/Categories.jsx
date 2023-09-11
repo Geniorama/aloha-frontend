9e77afab851f337f98782be0f9f947bd54507d6b
Jair Pérez <oquendojair10@gmail.com>
2023-09-11 11:27:39 -0500

    feat(Categories): add title prop

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,11 +2,11 @@ import TitleSection from "../TitleSection/TitleSection";
 import styles from "./Categories.module.css";
 import Category from "./Category/Category";
 
-export default function Categories({ categories }) {
+export default function Categories({ categories, title }) {
   return (
     <section className={styles.section}>
       <div className="container">
-        <TitleSection text="Categorías de fotos" />
+        <TitleSection text={`Categorías de ${title}`} />
         <div className={styles.content}>
           {categories.map((category, index) => (
             <Category key={index} {...category} />
