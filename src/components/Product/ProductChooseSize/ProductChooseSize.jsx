9ab0123b8683ef29b6918b4b7587860644a6f676
Jair Pérez <oquendojair10@gmail.com>
2023-09-13 10:01:04 -0500

    refactor(ProductChooseSize): make sizes list and add active radio handler

--- src/components/Product/ProductChooseSize/ProductChooseSize.jsx
+++ src/components/Product/ProductChooseSize/ProductChooseSize.jsx
@@ -1,40 +1,21 @@
+import { useState } from "react";
 import ProductChooseItem from "../ProductChooseItem/ProductChooseItem";
 import styles from "./ProductChooseSize.module.css";
+import sizes from "@/data/sizes.json";
 
 export default function ProductChooseSize() {
+  const [active, setActive] = useState(sizes[2].tag);
+  const onSetActiveRadio = (tag) => setActive(tag);
   return (
     <div className={styles.container}>
-      <ProductChooseItem
-        tag="S"
-        label="1000 x 668"
-        size="89.26x42.71"
-        dpi="300"
-      />
-      <ProductChooseItem
-        tag="L"
-        label="2000 x 1335"
-        size="89.26x42.71"
-        dpi="300"
-      />
-      <ProductChooseItem
-        tag="XL"
-        label="6016x4016"
-        size="89.26x42.71"
-        dpi="300"
-        active
-      />
-      <ProductChooseItem
-        tag="SUPER"
-        label="1000 x 668"
-        size="89.26x42.71"
-        dpi="300"
-      />
-      <ProductChooseItem
-        tag="EL"
-        label="1000 x 668"
-        size="89.26x42.71"
-        dpi="300"
-      />
+      {sizes.map((size, index) => (
+        <ProductChooseItem
+          key={index}
+          active={active === size.tag}
+          onSetActiveRadio={() => onSetActiveRadio(size.tag)}
+          {...size}
+        />
+      ))}
     </div>
   );
 }
