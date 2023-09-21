c7d858a877304698e75eb2e24333dcc71a56a387
Jair Pérez <oquendojair10@gmail.com>
2023-09-21 10:22:02 -0500

    fix(search.service): change return data

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -3,7 +3,7 @@ import { request } from "@/helpers/helpers";
 const search = async (search_query, options) => {
   try {
     const response = await request("search", { search_query, ...options });
-    return response.data;
+    return response;
   } catch (error) {
     console.log(error);
   }
