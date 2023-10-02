b15a5372bbd6a4bb9304900fc0175257d58d9df5
Jair Pérez <oquendojair10@gmail.com>
2023-10-02 12:35:03 -0500

    feat(HeroAuthor): change size to author image

--- src/components/HeroAuthor/HeroAuthor.jsx
+++ src/components/HeroAuthor/HeroAuthor.jsx
@@ -7,16 +7,17 @@ export default function HeroAuthor() {
     <section className={styles.hero}>
       <div className="container">
         <div className="row">
-          <div className="col-12 d-flex justify-content-center mb-4">
+          <div className="col-12 col-md-3 col-lg-2 d-flex justify-content-center align-items-center mb-4 mb-md-0">
             <Image
               src="/img/author/author.png"
               alt="author-avatar"
+              className={styles.image}
               quality={100}
-              height={100}
-              width={100}
+              height={160}
+              width={160}
             />
           </div>
-          <div className="col-12 d-flex flex-column justify-content-center text-center">
+          <div className="col-12 col-md-9 col-lg-7 d-flex flex-column justify-content-center align-items-md-start text-center text-md-start">
             <h1 className="fw-bold">Wavebreak Premium.</h1>
             <p>
               Our teams are constantly creating, coding and collaborating so we
@@ -24,7 +25,7 @@ export default function HeroAuthor() {
               imagery, customisable templates, graphic design, 4k, slow motion
               video and much more.
             </p>
-            <div className="d-flex justify-content-center gap-4">
+            <div className="d-flex justify-content-center justify-content-md-start gap-4">
               <ButtonLink text="Seguir" size="m" />
               <ButtonLink text="Compartir" size="m" />
             </div>
