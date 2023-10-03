0c8cb2c41581849e934882f118c8403d3e3637c8
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-10-03 16:12:15 -0500

    Create subcategories for vectors

---
+++ src/pages/categoria/vectores/[subcategoria].jsx
@@ -0,0 +1,45 @@
+import SubcategoryCard from "@/components/Categories/Subcategory/SubcategoryCard";
+import HeroCategory from "@/components/Hero/HeroCategory/HeroCategory";
+import Layout from "@/components/Layout/Layout";
+import subcategories from "@/data/subcategories_vector";
+
+export const getServerSideProps = async ({ params }) => {
+  try {
+    const subcategory = params.subcategoria;
+    const items = subcategories[params.subcategoria] || [];
+    return {
+      props: { items, subcategory },
+    };
+  } catch (error) {
+    return { props: { items: [] } };
+  }
+};
+
+function Subcatgory({ items = [], subcategory }) {
+  return (
+    <Layout metaData={{ title: `Subcategoría | ${subcategory}` }}>
+      <HeroCategory
+        background="/img/category/bg_subcat_vector.png"
+        search={true}
+        title={subcategory}
+        desc=" "
+        layer={{
+          background:
+            "linear-gradient(0deg, rgba(0,0,0,.7) 0%, rgba(0,0,0,0.3) 49%, rgba(0,0,0,0) 100%)",
+          backgroundBlendMode: "multiply",
+        }}
+      />
+      <section className="container p-5">
+        <div className="row">
+          {items.map((item, index) => (
+            <div key={index} className="col-4">
+              <SubcategoryCard url={`/search/fotos/${item.slug}`} {...item} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default Subcatgory;
