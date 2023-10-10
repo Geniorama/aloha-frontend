2a43c5a5048be59264b00ed1c62e798e92c55ce5
Jair Pérez <oquendojair10@gmail.com>
2023-10-10 18:41:27 -0500

    feat(autor): add integration with API

--- src/pages/autor/[autor].jsx
+++ src/pages/autor/[autor].jsx
@@ -2,19 +2,36 @@ import FilterOptions from "@/components/FilterOptions/FilterOptions";
 import GridGallery from "@/components/GridGallery/GridGallery";
 import HeroAuthor from "@/components/HeroAuthor/HeroAuthor";
 import Layout from "@/components/Layout/Layout";
+import search from "@/services/search.service";
+import { useEffect, useState } from "react";
 
 const metaData = {
   title: "Autor",
 };
 
-function AuthorPage() {
+function AuthorPage({ images }) {
+  const [author, setAuthor] = useState({});
+  useEffect(() => {
+    const item = images.length ? images[0] : {};
+    if (item) setAuthor({ username: item.username, avatar: item.avatar });
+  }, [images]);
   return (
     <Layout metaData={metaData}>
-      <HeroAuthor />
+      <HeroAuthor author={author} />
       <FilterOptions />
-      <GridGallery />
+      <GridGallery images={images} />
     </Layout>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const query = ctx.params.autor;
+  const response = await search("", {
+    search_user: query,
+    search_limit: 10,
+  });
+  const images = response?.result ?? {};
+  return { props: { images } };
+};
 
 export default AuthorPage;
