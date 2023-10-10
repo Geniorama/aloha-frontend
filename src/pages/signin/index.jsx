40d9ea8c7a8c9948a723a3cd9c7d44924c910968
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-10-09 21:13:14 -0500

    Create Login Page without responsive design

---
+++ src/pages/signin/index.jsx
@@ -0,0 +1,133 @@
+import styles from "../../styles/Signin.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import LogoBlack from "../../../public/img/components/header/logo-black-aloha.svg";
+import LogoGoogle from "../../../public/img/signup/logo-google.png";
+import LogoFB from "../../../public/img/signup/logo-fb.png";
+
+export default function Signin() {
+  return (
+    <div className={`${styles.section}`}>
+      <Link href="#" className={styles.logo}>
+        <Image src={LogoBlack} alt="Logo Aloha" />
+      </Link>
+      <div className={`${styles.content}`}>
+        <h1 className={`${styles.title}`}>Iniciar sesión en Aloha</h1>
+        <p className={`${styles.description}`}>
+          Ingresa a tu sesión con alguna de estas opciones
+        </p>
+        <div className={`${styles.loginButtons}`}>
+          <button className={`${styles.buttonGoogle}`}>
+            <span>
+              <Image src={LogoGoogle} alt="Sign up with Google" />
+            </span>
+            <span className={`${styles.ButtonSignUp__name} mx-2`}>
+              Continuar con Google
+            </span>
+            <span>
+              <svg
+                width="21"
+                height="20"
+                viewBox="0 0 21 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clip-path="url(#clip0_1016_36905)">
+                  <path
+                    d="M16.1963 9.99998L4.41115 9.99998"
+                    stroke="white"
+                    stroke-width="1.66667"
+                  />
+                  <path
+                    d="M10.8945 4.69849L16.1978 10.0018L10.8945 15.3051"
+                    stroke="white"
+                    stroke-width="1.66667"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_1016_36905">
+                    <rect
+                      width="20"
+                      height="20"
+                      fill="white"
+                      transform="translate(0.304688)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </span>
+          </button>
+          <button className={`${styles.buttonFacebook}`}>
+            <span>
+              <Image src={LogoFB} alt="Sign up with Facebook" />
+            </span>
+          </button>
+        </div>
+        <p className={`${styles.separador}`}>o ingresa con tu correo</p>
+        <form className={`${styles.formLogin}`}>
+          <label className={`${styles.formLabel}`}>Correo electrónico *</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Ingresa tu correo electrónico"
+            className={`${styles.formInput}`}
+          />
+          <div className={`${styles.formContentLabels}`}>
+            <label className={`${styles.formLabel}`}>Contraseña *</label>
+            <label className={`${styles.formRememberPass}`}>
+              ¿Olvidaste tu contraseña?
+            </label>
+          </div>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Ingresa tu contraseña"
+            className={`${styles.formInput}`}
+          />
+          <button className={`${styles.buttonGoogle} my-3`}>
+            <span className={`${styles.ButtonSignUp__name} mx-2`}>
+              Ingresar
+            </span>
+            <span>
+              <svg
+                width="21"
+                height="20"
+                viewBox="0 0 21 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clip-path="url(#clip0_1016_36905)">
+                  <path
+                    d="M16.1963 9.99998L4.41115 9.99998"
+                    stroke="white"
+                    stroke-width="1.66667"
+                  />
+                  <path
+                    d="M10.8945 4.69849L16.1978 10.0018L10.8945 15.3051"
+                    stroke="white"
+                    stroke-width="1.66667"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_1016_36905">
+                    <rect
+                      width="20"
+                      height="20"
+                      fill="white"
+                      transform="translate(0.304688)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </span>
+          </button>
+        </form>
+        <p className={`${styles.registrate}`}>
+          ¿Aún no tienes cuenta? <span>¡Regístrate!</span>
+        </p>
+      </div>
+    </div>
+  );
+}
