2640c1b721e2e863f060f9bada20c9b19ef892be
Jair Pérez <oquendojair10@gmail.com>
2023-10-11 13:59:19 -0500

    feat(user.service): add get user data and set user and session cookie

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,29 @@
 import { request } from "@/helpers/helpers";
+import { getCookie, setCookie } from "cookies-next";
+import Router from "next/router";
 
 export const loginAsUser = async (login_user, login_password) => {
   try {
     const response = await request("loginAsUser", {
       login_user,
       login_password,
+    });
+    setCookie("user_id", response.userid);
+    setCookie("session_id", response.sessionid);
+    Router.push("/user");
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getUserData = async () => {
+  try {
+    const user_id = getCookie("user_id");
+    const session_id = getCookie("session_id");
+    const response = await request("getUserData", {
+      user_id,
+      session_id,
     });
     return response;
   } catch (error) {
