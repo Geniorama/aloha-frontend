4889e71e7932e9370835eaccb7d000b24632f3c2
Jair Pérez <oquendojair10@gmail.com>
2023-10-12 11:28:29 -0500

    fix(GridGallery): change size for orientation item

--- src/components/GridGallery/GridGallery.jsx
+++ src/components/GridGallery/GridGallery.jsx
@@ -3,21 +3,52 @@ import styles from "./GirdGallery.module.css";
 import Link from "next/link";
 
 export default function GridGallery({ images = [] }) {
+  const getOrientation = (image) => {
+    const sizes = {
+      horizontal: {
+        width: 400,
+        height: 250,
+      },
+      vertical: {
+        width: 250,
+        height: 500,
+      },
+      square: { width: 400, height: 400 },
+    };
+    if (parseInt(image.width) > parseInt(image.height)) return sizes.horizontal;
+    if (parseInt(image.width) === parseInt(image.height)) return sizes.square;
+    if (parseInt(image.width) < parseInt(image.height)) return sizes.vertical;
+  };
   return (
     <div className={`container ${styles.GridGallery}`}>
-      {images.map((image, index) => (
-        <Link key={image.id} href={`/producto/${image.id}`}>
-          <Image
-            key={index}
-            src={image.url_big}
-            className={styles.Image}
-            width={1000}
-            quality={100}
-            height={image.height * 0.1 < 600 ? image.height * 0.1 : 500}
-            alt=""
-          />
-        </Link>
-      ))}
+      {images.map((image, index) => {
+        const orientation = getOrientation(image);
+        console.log(
+          "orientation",
+          orientation,
+          "width",
+          image.width,
+          "height",
+          image.height,
+          index,
+          image.huge_thumb
+        );
+        return (
+          <Link key={image.id} href={`/producto/${image.id}`}>
+            <Image
+              key={index}
+              src={image.huge_thumb}
+              objectFit="cover"
+              objectPosition="top"
+              className={styles.Image}
+              width={orientation.width}
+              height={orientation.height}
+              quality={100}
+              alt=""
+            />
+          </Link>
+        );
+      })}
     </div>
   );
 }
