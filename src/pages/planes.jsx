4730a367b2c2dc8d63ef2f610bb121fc623d8a59
Jair Pérez <oquendojair10@gmail.com>
2023-10-23 06:44:22 -0500

    feat: create planes page

---
+++ src/pages/planes.jsx
@@ -0,0 +1,98 @@
+import Layout from "@/components/Layout/Layout";
+import PlanSection from "@/components/PlanSection/PlanSection";
+import styles from "@/styles/Planes.module.css";
+import Image from "next/image";
+
+const metaData = {
+  title: "Planes",
+  description: "Mi metadescripción",
+  keywords: "Banco imágenes, aloha",
+  author: "Geniorama Agencia",
+};
+
+function PlanesPage() {
+  return (
+    <Layout metaData={metaData}>
+      <PlanSection />
+      <section className={styles.Banner}>
+        <div>
+          <h2>¿Necesitas una solución personalizada?</h2>
+          <p>
+            Podemos personalizar nuestros planes según tus necesidades.
+            <br /> ¡Ponte en contacto con nosotros para concretar precios y
+            condiciones!
+          </p>
+          <button type="submit" target="_self">
+            <Image src="/icons/Whatsapp.svg" width={25} height={25} alt="" />
+            <span>Escríbenos</span>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clipPath="url(#clip0_48_55)">
+                <path
+                  d="M22.6668 9.33301L9.3335 22.6663"
+                  stroke="black"
+                  strokeWidth="2.66667"
+                />
+                <path
+                  d="M10.6665 9.33301H22.6665V21.333"
+                  stroke="black"
+                  strokeWidth="2.66667"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_48_55">
+                  <rect width="32" height="32" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </button>
+        </div>
+      </section>
+      <section className={styles.BannerBusiness}>
+        <div className={styles.BannerBusinessBackground} />
+        <div className={styles.BannerBusinessContent}>
+          <h2>¿Necesitas un plan para tu empresa?</h2>
+          <p>
+            Eleva el esfuerzo de tu equipo al siguiente nivel con nuestros
+            planes para empresa
+          </p>
+          <button type="submit" target="_self">
+            <span>Ver planes para empresas</span>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clipPath="url(#clip0_48_55)">
+                <path
+                  d="M22.6668 9.33301L9.3335 22.6663"
+                  stroke="black"
+                  strokeWidth="2.66667"
+                />
+                <path
+                  d="M10.6665 9.33301H22.6665V21.333"
+                  stroke="black"
+                  strokeWidth="2.66667"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_48_55">
+                  <rect width="32" height="32" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </button>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default PlanesPage;
