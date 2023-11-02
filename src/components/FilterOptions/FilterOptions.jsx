47a8e38ed829966a1f7e0139c4df2dc4ab833616
Jair Pérez <oquendojair10@gmail.com>
2023-11-02 09:59:16 -0500

    style: add field and lables classNames

--- src/components/FilterOptions/FilterOptions.jsx
+++ src/components/FilterOptions/FilterOptions.jsx
@@ -1,23 +1,24 @@
 import styles from "./FilterOptions.module.css";
 import SearchFilter from "../SearchFilter/SearchFilter";
-import FilterOptionsTags from "./FilterOptionsTags/FilterOptionsTags";
+import FilterLabels from "./FilterLabels/FilterLabels";
 
 export default function FilterOptions() {
   return (
-    <section className={styles.FilterOptions}>
-      <div className="container my-4">
+    <section className={styles.filterOptions}>
+      <div className="container py-4">
         <div className="row">
           <div className="col-12 col-lg-6">
             <SearchFilter />
           </div>
-          <div className="col-6 col-lg-6 d-flex gap-2 align-items-center justify-content-end">
+          <div className={`col-8 col-lg-6 ${styles.field}`}>
             <span>Ordenar por</span>
             <select name="" id="">
               <option value="">Más relevante</option>
             </select>
           </div>
-          <div className="col-6 col-lg-12">
-            <FilterOptionsTags />
+          <div className={`col-4 col-lg-12 ${styles.labels}`}>
+            <span>Filtrar</span>
+            <FilterLabels />
           </div>
         </div>
       </div>
