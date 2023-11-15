19d57f93f0200642c376955bc65bef009218118d
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-11-15 09:56:56 -0500

    feat: update music sfx page, add layout sections

--- src/components/TitleSection/TitleSection.jsx
+++ src/components/TitleSection/TitleSection.jsx
@@ -1,5 +1,9 @@
 import styles from "./TitleSection.module.css";
 
-export default function TitleSection({ text, color }) {
-  return <h2 className={`${styles[color]} ${styles.TitleSection}`}>{text}</h2>;
+export default function TitleSection({ text, color, otherClass = "" }) {
+  return (
+    <h2 className={`${styles[color]} ${otherClass} ${styles.TitleSection}`}>
+      {text}
+    </h2>
+  );
 }
--- src/pages/musica-sfx.jsx
+++ src/pages/musica-sfx.jsx
@@ -1,7 +1,12 @@
+import ButtonLink from "@/components/ButtonLink/ButtonLink";
 import CardGrid from "@/components/CardGrid/CardGrid";
 import HeroCategory from "@/components/Hero/HeroCategory/HeroCategory";
 import Layout from "@/components/Layout/Layout";
+import PlanSection from "@/components/PlanSection/PlanSection";
+import PopularAuthors from "@/components/PopularAuthors/PopularAuthors";
+import PopularSearches from "@/components/PupularSearches/PopularSearches";
 import Slider from "@/components/Slider/Slider";
+import TitleSection from "@/components/TitleSection/TitleSection";
 const metaData = {
   title: "Música y SFX",
 };
@@ -19,17 +24,34 @@ function MusicSFXPage() {
           backgroundBlendMode: "multiply",
         }}
       />
-      <div>
-        <Slider>
-          <CardGrid
-            name="Alternativa"
-            items={["/img/music-sfx/alternative.png"]}
-          />
-          <CardGrid name="Ambiente" items={["/img/music-sfx/ambiente.png"]} />
-          <CardGrid name="Cine" items={["/img/music-sfx/cine.png"]} />
-          <CardGrid items={["/img/music-sfx/alternative.png"]} />
-        </Slider>
+      <div className="section py-5">
+        <div className="container py-3">
+          <TitleSection text={`Categorías de música.`} />
+          <div className="text-center mb-4">
+            <ButtonLink text={"Ver todos"} color={"coral"} size={"m"} />
+          </div>
+          <Slider>
+            <CardGrid
+              name="Alternativa"
+              items={["/img/music-sfx/alternative.png"]}
+            />
+            <CardGrid name="Ambiente" items={["/img/music-sfx/ambiente.png"]} />
+            <CardGrid name="Cine" items={["/img/music-sfx/cine.png"]} />
+            <CardGrid items={["/img/music-sfx/alternative.png"]} />
+          </Slider>
+        </div>
       </div>
+      <PopularAuthors type="dark" />
+      <div className="section py-5">
+        <div className="container py-1">
+          <TitleSection
+            otherClass="text-wrap-balance"
+            text={`Descubres las tendencias actuales.`}
+          />
+        </div>
+      </div>
+      <PlanSection />
+      <PopularSearches />
     </Layout>
   );
 }
