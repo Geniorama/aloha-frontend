97a1ef3d5295c73872289d77e1026dbba3498ab2
Jair Pérez <oquendojair10@gmail.com>
2023-11-20 15:25:40 -0500

    feat(SubMenuDesktop): add path to items

--- src/components/SubMenuDesktop/SubMenuDesktop.jsx
+++ src/components/SubMenuDesktop/SubMenuDesktop.jsx
@@ -50,168 +50,174 @@ export default function SubMenuDesktop({ isHeaderFixed }) {
           <span className={styles.Tag}>Fotos</span>
         </div>
       </Link>
-      <div className={`${styles.GridThree} ${styles.SubMenuDesktopItem}`}>
-        <div
-          style={{
-            gridColumn: "span 1",
-            gridRow: "span 1",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/vector.jpg"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
+      <Link href="/categoria/vectores">
+        <div className={`${styles.GridThree} ${styles.SubMenuDesktopItem}`}>
+          <div
+            style={{
+              gridColumn: "span 1",
+              gridRow: "span 1",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/vector.jpg"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 1",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/covid-19.jpg"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 1",
+              gridRow: "span 2",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/futurista.jpg"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 2",
+              gridRow: "span 1",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/stock-vector.jpg"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <span className={styles.Tag}>Vectores</span>
         </div>
-        <div
-          style={{
-            gridColumn: "span 1",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/covid-19.jpg"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
+      </Link>
+      <Link href="/categoria/ilustraciones">
+        <div className={`${styles.GridFour} ${styles.SubMenuDesktopItem}`}>
+          <div
+            style={{
+              gridColumn: "span 1",
+              gridRow: "span 2",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/bg_subcat_moderno.png"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 1",
+              gridRow: "span 2",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/bg_subcat_moderno.png"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 2",
+              gridRow: "span 5",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/bg_subcat_moderno.png"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 2",
+              gridRow: "span 2",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/bg_subcat_moderno.png"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <span className={styles.Tag}>Ilustraciones</span>
         </div>
-        <div
-          style={{
-            gridColumn: "span 1",
-            gridRow: "span 2",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/futurista.jpg"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
+      </Link>
+      <Link href="/colecciones">
+        <div className={`${styles.GridFour} ${styles.SubMenuDesktopItem}`}>
+          <div
+            style={{
+              gridColumn: "span 2",
+              gridRow: "span 2",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/bg_subcat_naturaleza.png"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 2",
+              gridRow: "span 5",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/bg_subcat_naturaleza.png"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <div
+            style={{
+              gridColumn: "span 2",
+              gridRow: "span 2",
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/img/category/bg_subcat_naturaleza.png"
+              objectFit="cover"
+              alt="cover"
+              fill
+            />
+          </div>
+          <span className={styles.Tag}>Colecciones</span>
         </div>
-        <div
-          style={{
-            gridColumn: "span 2",
-            gridRow: "span 1",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/stock-vector.jpg"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <span className={styles.Tag}>Vectores</span>
-      </div>
-      <div className={`${styles.GridFour} ${styles.SubMenuDesktopItem}`}>
-        <div
-          style={{
-            gridColumn: "span 1",
-            gridRow: "span 2",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/bg_subcat_moderno.png"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <div
-          style={{
-            gridColumn: "span 1",
-            gridRow: "span 2",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/bg_subcat_moderno.png"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <div
-          style={{
-            gridColumn: "span 2",
-            gridRow: "span 5",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/bg_subcat_moderno.png"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <div
-          style={{
-            gridColumn: "span 2",
-            gridRow: "span 2",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/bg_subcat_moderno.png"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <span className={styles.Tag}>Ilustraciones</span>
-      </div>
-      <div className={`${styles.GridFour} ${styles.SubMenuDesktopItem}`}>
-        <div
-          style={{
-            gridColumn: "span 2",
-            gridRow: "span 2",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/bg_subcat_naturaleza.png"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <div
-          style={{
-            gridColumn: "span 2",
-            gridRow: "span 5",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/bg_subcat_naturaleza.png"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <div
-          style={{
-            gridColumn: "span 2",
-            gridRow: "span 2",
-            position: "relative",
-          }}
-        >
-          <Image
-            src="/img/category/bg_subcat_naturaleza.png"
-            objectFit="cover"
-            alt="cover"
-            fill
-          />
-        </div>
-        <span className={styles.Tag}>Colecciones</span>
-      </div>
+      </Link>
     </div>
   );
 }
